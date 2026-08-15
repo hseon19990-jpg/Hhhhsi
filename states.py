9f83541d8b53cca006d667d0ee9ad76d3f13a57f
Mohamm0872 <hseon19990@gmail.com>
2026-08-16 00:14:21 +0300

    Enhance user state management with new session states
    Added new states for session management and updated temporary data structure.

--- states.py
+++ states.py
@@ -1,8 +1,6 @@
-# تخزين حالات المستخدمين مؤقتاً
 user_states = {}
-user_temp_data = {}  # لتخزين بيانات مؤقتة
+user_temp_data = {}
 
-# أنواع الحالات
 STATE_NONE = 0
 STATE_WAITING_ACCOUNT = 1
 STATE_WAITING_CLIP = 2
@@ -12,33 +10,31 @@ STATE_WAITING_DELETE_GROUP = 5
 STATE_WAITING_DELETE_CLIP = 6
 STATE_WAITING_TIMER = 7
 STATE_WAITING_CONFIRM = 8
+STATE_WAITING_SESSION = 9
+STATE_WAITING_SESSION_PHONE = 10
+STATE_WAITING_ROTATE = 11
+STATE_WAITING_CHATS = 12
 
 def set_state(user_id, state):
-    """تعيين حالة لمستخدم"""
     user_states[user_id] = state
 
 def get_state(user_id):
-    """الحصول على حالة مستخدم"""
     return user_states.get(user_id, STATE_NONE)
 
 def clear_state(user_id):
-    """مسح حالة مستخدم"""
     if user_id in user_states:
         del user_states[user_id]
     if user_id in user_temp_data:
         del user_temp_data[user_id]
 
 def set_temp_data(user_id, key, value):
-    """تخزين بيانات مؤقتة للمستخدم"""
     if user_id not in user_temp_data:
         user_temp_data[user_id] = {}
     user_temp_data[user_id][key] = value
 
 def get_temp_data(user_id, key):
-    """الحصول على بيانات مؤقتة للمستخدم"""
     return user_temp_data.get(user_id, {}).get(key)
 
 def clear_temp_data(user_id):
-    """مسح البيانات المؤقتة"""
     if user_id in user_temp_data:
         del user_temp_data[user_id]
